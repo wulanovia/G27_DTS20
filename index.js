98e1329ccd6ea3b0fbb5b541122ac52fbcd759c5
mnurfilza <choirfilza@gmail.com>
2020-04-27 09:27:23 +0700

    update lagi

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function getResolution() {
 let desktopMessage = document.querySelector(".message-wrapper");
 
     if (screen.width < 1000) {
-        desktopMessage.querySelector(".container-about").querySelectorAll(".msg").forEach(element => {
+        desktopMessage.querySelector(".container-msg").querySelectorAll(".msg").forEach(element => {
             element.remove()
         });
 
-        desktopMessage.querySelector(".container-about").innerHTML = '<div class="content-right msg"><img class="vector-pic"src="img/3411109.jpg" alt=""></div> <div class="content-left msg"><div class="question"><h2>Apakah anda siap untuk menjadi programer yang handal?</h2><h3>Ayo belajar coding, ayo belajar menjadi lebih kreatif!</h3><span class="btn message">Mulai Belajar</span></div></div>'
+        desktopMessage.querySelector(".container-msg").innerHTML = '<div class="content-right msg"><img class="vector-pic"src="img/3411109.jpg" alt=""></div> <div class="content-left msg"><div class="question"><h2>Apakah anda siap untuk menjadi programer yang handal?</h2><h3>Ayo belajar coding, ayo belajar menjadi lebih kreatif!</h3><span class="btn message">Mulai Belajar</span></div></div>'
 
     } 
     
